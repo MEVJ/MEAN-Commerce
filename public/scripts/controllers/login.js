4b0424a7bb47a8a9fefa1bdb6010343f04e0814c
MeanVJ <viju1985@gmail.com>
2015-03-17 16:42:00 +0800

    Initial commit

---
+++ public/scripts/controllers/login.js
@@ -0,0 +1,27 @@
+'use strict';
+
+angular.module('psJwtApp').controller('LoginCtrl', function ($scope, alert, auth, $auth) {
+	$scope.submit = function () {
+		$auth.login({
+			email: $scope.email,
+			password: $scope.password
+		}).then(function (res) {
+			var message = 'Thanks for coming back ' + res.data.user.email + '!';
+
+			if (!res.data.user.active)
+				message = 'Just a reminder, please activate your account soon :)';
+
+			alert('success', 'Welcome', message);
+		}).catch(handleError);
+	};
+
+	$scope.authenticate = function (provider) {
+		$auth.authenticate(provider).then(function (res) {
+			alert('success', 'Welcome', 'Thanks for coming back ' + res.data.user.displayName + '!');
+		}, handleError);
+	}
+
+	function handleError(err) {
+		alert('warning', 'Something went wrong :(', err.message);
+	}
+});
