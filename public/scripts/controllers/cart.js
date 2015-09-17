104548f79a42a3ec51212a6d18071cd6cbcb2788
Vijay Babu <viju1985@gmail.com>
2015-09-17 18:08:51 +0800

    Update cart.js

--- public/scripts/controllers/cart.js
+++ public/scripts/controllers/cart.js
@@ -3,8 +3,9 @@
 angular.module('Mean-Commerce')
 	.controller('CartCtrl', function ($scope, $http, API_URL, alert) {
 
-		$http.get('http://localhost:1337/cart').success(function (cart) {
+		$http.get('http://localhost:3000/content.json').success(function (cart) {
 			$scope.cart = cart;
+			console.log('getting the cart value '+ cart);
 		}).error(function (err) {
 			alert('warning', "Unable to get cart", err.message);
 		})
